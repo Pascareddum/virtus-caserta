bd9fd4c8a9aa8fd0a040999b78ca7092e4d49ac0
Alessandro Pascarella <109096677+Pascareddum@users.noreply.github.com>
2026-04-17 10:29:32 +0200

    fix email pt.2

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns        = require('dns');
+dns.setDefaultResultOrder('ipv4first'); // Railway non supporta IPv6 in uscita
 const express    = require('express');
 const fetch      = require('node-fetch');
 const path       = require('path');
